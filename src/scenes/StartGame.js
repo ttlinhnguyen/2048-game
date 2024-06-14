606e4476b834107ff3e5cb1024ced78b11f02535
Linh Nguyen <linhtunguyen.1311@gmail.com>
2024-06-14 20:38:03 +0930

    Use import phaser in StartGame

--- src/scenes/StartGame.js
+++ src/scenes/StartGame.js
@@ -1,29 +1,36 @@
 import { gameOptions } from "../commonSettings.js";
-var StartGame = new Phaser.Class({
-    Extends: Phaser.Scene,
-    initialize:
-        function startGame() {
-            Phaser.Scene.call(this, { key: "StartGame" });
-        },
+import { Scene, Class } from "phaser";
+
+var StartGame = new Class({
+    Extends: Scene,
+    initialize: function startGame() {
+        Scene.call(this, { key: "StartGame" });
+    },
     create: function () {
-        this.music = this.sound.add("main_audio", { loop: true })
-        this.music.play()
-        this.add.image(450, 530, "startbg")
-        this.startButton = this.add.image(450, 760, "startbtn").setScale(0.7).setInteractive()
-        this.startButton.on("pointerdown", function (pointer) {
-            this.tweens.add({
-                targets: [this.startButton],
-                scaleX: 1.005,
-                scaleY: 1.005,
-                duration: gameOptions.tweenSpeed,
-                yoyo: true,
-                repeat: 1,
-                onComplete: function (tween) {
-                    tween.parent.scene.scene.start("PlayGame")
-                    }
-            });
-        }, this)
-        
-    }
+        this.music = this.sound.add("main_audio", { loop: true });
+        this.music.play();
+        this.add.image(450, 530, "startbg");
+        this.startButton = this.add
+            .image(450, 760, "startbtn")
+            .setScale(0.7)
+            .setInteractive();
+        this.startButton.on(
+            "pointerdown",
+            function () {
+                this.tweens.add({
+                    targets: [this.startButton],
+                    scaleX: 1.005,
+                    scaleY: 1.005,
+                    duration: gameOptions.tweenSpeed,
+                    yoyo: true,
+                    repeat: 1,
+                    onComplete: function (tween) {
+                        tween.parent.scene.scene.start("PlayGame");
+                    },
+                });
+            },
+            this
+        );
+    },
 });
 export default StartGame;

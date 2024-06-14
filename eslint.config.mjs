867243b5655c956ef8bac9e24b8c3b399334abca
Linh Nguyen <linhtunguyen.1311@gmail.com>
2024-06-14 22:41:13 +0930

    Update eslint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,12 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+    {
+        languageOptions: { globals: globals.browser },
+        env: {
+            node: true,
+        },
+    },
+    pluginJs.configs.recommended,
 ];
